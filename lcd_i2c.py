26ad6f7e1537a4645a6f44d515f4517fe8c602de
vladkozlov69 <43416162+vladkozlov69@users.noreply.github.com>
2020-11-20 14:01:08 +0200

    Updated i2c address

--- lcd_i2c.py
+++ lcd_i2c.py
@@ -15,7 +15,7 @@ import signal
 from datetime import datetime
 
 # Define some device parameters
-I2C_ADDR  = 0x27 # I2C device address
+I2C_ADDR  = 0x3F # 0x27 # I2C device address
 LCD_WIDTH = 16   # Maximum characters per line
 
 # Define some device constants
